9ca2ed325ee86dd1b313758abb89590eae2e18f2
Jasmine <jasmineuchil@gmail.com>
2021-05-18 15:35:04 +0530

    Update pythoncode.py

--- pythoncode.py
+++ pythoncode.py
@@ -1,8 +1,8 @@
 import pymongo
 from pymongo import MongoClient
 from random import randint
-#Step 1: Connect to MongoDB 
-client = MongoClient("mongodb://myUserAdmin:password@p1213-master.p1213.cecc.ihost.com:31466")
+#Step 1: Connect to MongoDB
+client = MongoClient("mongodb://myUserAdmin:password@HOSTNAME:NODEPORT")
 db=client.newdb
 #Step 2: Create sample data
 names = ['Kitchen','Animal','State', 'Tastey', 'Big','City','Fish', 'Pizza','Goat', 'Salty','Sandwich','Lazy', 'Fun']
