f56017f30d430ca5b0fe750f06f6c9fd73632384
Jasmine <jasmineuchil@gmail.com>
2021-05-18 13:32:13 +0530

    Update dblist.py

--- dblist.py
+++ dblist.py
@@ -1,5 +1,4 @@
 from pymongo import MongoClient
-from pprint import pprint
 client = MongoClient("mongodb://myUserAdmin:password@p1213-master.p1213.cecc.ihost.com:31466")
 for db in client.list_databases():
     print(db)
