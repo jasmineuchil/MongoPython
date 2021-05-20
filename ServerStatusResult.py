51ac69bc9f3f1f930d169606a9ecf69478bdf0f5
Jasmine <jasmineuchil@gmail.com>
2021-05-20 11:16:59 +0530

    Delete ServerStatusResult.py

--- ServerStatusResult.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-from pprint import pprint
-client = MongoClient("mongodb://myUserAdmin:password@HOSTNAME:NODEPORT")
-db=client.admin
-# Issue the serverStatus command and print the results
-serverStatusResult=db.command("serverStatus")
-pprint(serverStatusResult)
