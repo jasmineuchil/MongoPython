4ab1f9e9e0168640ffad906686e3575959eabfb7
Jasmine <jasmineuchil@gmail.com>
2021-05-25 12:15:10 +0530

    Update serverstatus.py

--- serverstatus.py
+++ serverstatus.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from pprint import pprint
-client = MongoClient("mongodb://myUserAdmin:password@HOSTNAME:NODEPORT")
+client = MongoClient("mongodb://Username:password@HOSTNAME:NODEPORT")
 db=client.admin
 # Issue the serverStatus command and print the results
 serverStatusResult=db.command("serverStatus")
