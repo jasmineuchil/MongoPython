d89853e22af9d9942052381ea8401658f78a33ad
Jasmine <jasmineuchil@gmail.com>
2021-06-15 12:32:40 +0530

    Update read.py

--- read.py
+++ read.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 client = MongoClient("mongodb://Username:password@HOSTNAME:NODEPORT")
 #Go to the database which is already created and prin the data
-db = client["newdatabase"]
-col = db["newcollection"]
+db = client["newdb"]
+col = db["reviews"]
 x = col.find()
 for data in x:
     print(data)
